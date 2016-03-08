e293bbda785cdd4e97500ba788b9ce033a98b4d7
BurningNetel <joepseuren1@gmail.com>
2016-03-08 11:45:07 +0100

    Formatting and comments

--- WoordenApplicatie/src/woordenapplicatie/gui/WoordenController.java
+++ WoordenApplicatie/src/woordenapplicatie/gui/WoordenController.java
@@ -48,27 +48,37 @@ public class WoordenController implements Initializable {
     }
 
     /**
-     * Removes ',' and newlines, lowers and finally splits the input text into an string array.
+     * Removes ',' and newlines, lowers and finally splits the input text into an array of words.
      * @return
      */
     private String[] getInputAsArray(){
         return taInput.getText().replace(",","").replace("\n"," ").toLowerCase().split("\\s");
     }
 
+    /**
+     * Counts the amount of words and the unique words of the input text.
+     * @param event
+     */
     @FXML
     private void aantalAction(ActionEvent event) {
         String[] list = this.getInputAsArray();
-        String text;
+        StringBuilder sb = new StringBuilder();
 
-        text = "Totaal aantal woorden: " + String.valueOf(list.length) + "\n";
+        sb.append("Totaal aantal woorden: " + list.length + "\n");
 
+        // Filter duplicates by using a set.
         Set<String> set = new HashSet<>(Arrays.asList(list));
 
-        text += "Aantal verschillende woorden: " + set.size() + "\n";
+        sb.append("Aantal verschillende woorden: " + set.size() + "\n");
 
-        taOutput.setText(text);
+        taOutput.setText(sb.toString());
     }
 
+
+    /**
+     * Sorts the input text alphabetically in reverse order (z-a)
+     * @param event
+     */
     @FXML
     private void sorteerAction(ActionEvent event) {
         String[] list = this.getInputAsArray();
@@ -81,16 +91,19 @@ public class WoordenController implements Initializable {
         set.descendingSet().forEach(s -> taOutput.appendText(s + "\n"));
     }
 
+    /**
+     * Sort the input text on number of occurrences inside the text.
+     */
     @FXML
     private void frequentieAction(ActionEvent event) {
-        String[] list = this.getInputAsArray();
+        String[] words = this.getInputAsArray();
 
         Map<String, Integer> wordCountMap = new HashMap<>();
 
         // Put in a new key, or increment the existing value by 1 if the key exists
         // O(n)
-        for (String s : list) {
-            wordCountMap.put(s, wordCountMap.containsKey(s) ? wordCountMap.get(s) + 1 : 1);
+        for (String word : words) {
+            wordCountMap.put(word, wordCountMap.containsKey(word) ? wordCountMap.get(word) + 1 : 1);
         }
 
         taOutput.clear();
@@ -103,30 +116,36 @@ public class WoordenController implements Initializable {
 
     }
 
+    /**
+     * For each word in the input text, print on which row it is located.
+     */
     @FXML
     private void concordatieAction(ActionEvent event) {
         String[] words = this.getInputAsArray();
 
         Set<String> wordsSet = new HashSet<>(Arrays.asList(words));
 
-        // rows
-        LinkedList<String> rowList = new LinkedList<>(Arrays.asList(taInput.getText().replaceAll(",","").toLowerCase().split("\n")));
+        // Split the input text into a list of rows
+        LinkedList<String> rows = new LinkedList<>(
+                Arrays.asList(taInput.getText().replaceAll(",","").toLowerCase().split("\n"))
+        );
 
+        // A map that holds the location of the words
         Map<String, HashSet<Integer>> wordLocation = new HashMap<>();
 
-        // O(n2)
+        // ~O(n2) (This one is hard to decide, because it depends on the number of rows)
+        // Loop trough each word and find the words where the word is located.
         for (String word : wordsSet) {
-            // Loop trough each row and update the map accordingly
-            int rowcount = 0;
+            int rowPointer = 0;
 
-            for (int row = 0; row < rowList.size(); row++) {
-                int count = ++rowcount;
+            for (String row : rows) {
+                int count = ++rowPointer;
 
                 // Adds the rows location to the array if the word is in the row
-                if (rowList.get(row).contains(word)) {
+                if (row.contains(word)) {
                     if (wordLocation.get(word) != null) {
                         wordLocation.get(word).add(count);
-                     } else {
+                    } else {
                         wordLocation.put(word, new HashSet<>(Arrays.asList(count)));
                     }
                 }
